46c5dd7e2cccebb7ba8799533c1d4256b980aa49
Scarlett Patton <scarwars05@gmail.com>
2018-06-20 20:24:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Write your code in this file!
 
-const currentUser = 'Scarlett Patton';
+const currentUser = 'Scarlett Patton'.charAt(0);
 
 const welcomeMessage = `Welcome to Klossybook, ${currentUser}!`;
 
